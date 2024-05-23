b7795bbe80e242867b851da81f80809a0e0ae64d
letzdev.jh <letzdev.jh@gmail.com>
2024-05-23 22:28:01 +0900

    Revert "chore: domain layer module android module -> kotlin module 변경"
    This reverts commit 864db1b9d56c070a09402da9e43d7b0e81427dae.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
     id(Plugins.KOTLIN_ANDROID) version Versions.KOTLIN_ANDROID apply false
     id(Plugins.ANDROID_LIBRARY) version Versions.ANDROID_LIBRARY apply false
     id(Plugins.DAGGER_HILT) version Versions.DAGGER_HILT apply false
+
 }
--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,9 +1,44 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id(Plugins.ANDROID_LIBRARY)
+    id(Plugins.KOTLIN_ANDROID)
 }
 
-java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+android {
+    namespace = "${DefaultConfig.NAME_SPACE}.domain"
+    compileSdk = DefaultConfig.COMPILE_SDK_VERSION
+
+    defaultConfig {
+        minSdk = DefaultConfig.MIN_SDK_VERSION
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // Android
+    implementation(Dependencies.ANDROID_CORE_KTX)
+    implementation(Dependencies.ANDROID_APPCOMPAT)
+    implementation(Dependencies.ANDROID_MATERIAL)
+    // Test
+    testImplementation(Dependencies.JUNIT)
+    androidTestImplementation(Dependencies.ANDROID_TEST_JUNIT)
+    androidTestImplementation(Dependencies.ANDROID_TEST_ESPRESSO)
 }
